a3698054e3e15792cbbf9cd6f72e6e819e3e51a5
Lucca Carvalho <luccalafonte97@gmail.com>
2019-03-09 21:35:15 -0300

    Adding panini scrapper

---
+++ paniniScrapper.js
@@ -0,0 +1,52 @@
+const rp = require('request-promise');
+const $ = require('cheerio');
+const saveInMonth = require('./saveInMonth');
+
+let url = process.argv[2];
+const month = process.argv[3];
+const baseUrl = 'https://loja.panini.com.br';
+
+const addMangasToArray = (mangas, html) => {
+    const mangasSelector = $('.row', html).find('.description > h4 > a');
+    const mangasLength = mangasSelector.length;
+
+    for(let i=0; i<mangasLength; i++){
+        mangas.push(mangasSelector[i].children[0].data);
+    }
+};
+
+rp(url)
+    .then((html) => {
+        const urls = [url]
+
+        const pagSelector = $('.results > #ConteudoBodyMaster_ConteudoCorpo_CtlResultadoBusca_DataPager1 > a.pagingLink', html);
+        const pagQuant = pagSelector.length;
+        for(let i=0; i<pagQuant; i++){
+            url = baseUrl + pagSelector[i].attribs.href;
+            urls.push(url);
+        }
+        return urls;
+    })
+    .then((urls) => {
+        const promises = [];
+
+        urls.forEach((url) => {
+            promises.push(rp(url));
+        });
+
+        Promise.all(promises)
+            .then((responses) => {
+                const mangas = [];
+
+                responses.forEach((html) => {
+                    addMangasToArray(mangas, html);
+                });
+
+                return mangas;
+            })
+            .then((mangas) => saveInMonth(mangas, month))
+            .catch((err) => console.log(err));
+    })
+    .catch((err) => {
+        console.log(err);
+    });
